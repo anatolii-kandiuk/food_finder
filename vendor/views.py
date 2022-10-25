75d24ccb32170209b73c5379b2f172170ba3e016
anatolii <batkovich327@gmail.com>
2022-10-25 18:33:19 +0300

    [FF] - Create vendor profile form and validation

--- vendor/views.py
+++ vendor/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+
+from accounts.forms import UserProfileForm
+from accounts.models import UserProfile
+from accounts.views import check_role_vendor
+
+from .forms import VendorForm
+from .models import Vendor
 
 
+@user_passes_test(check_role_vendor)
+@login_required(login_url='login')
 def vprofile(request):
-    return render(request, 'vendor/vprofile.html')
+    profile = get_object_or_404(UserProfile, user=request.user)
+    vendor = get_object_or_404(Vendor, user=request.user)
+
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        vendor_form = VendorForm(request.POST, request.FILES, instance=profile)
+
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, 'Settings updated')
+            return redirect('vprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        vendor_form = VendorForm(instance=vendor)
+
+    context = {
+        'profile_form': profile_form,
+        'vendor_form': vendor_form,
+        'profile': profile,
+        'vendor': vendor,
+    }
+    return render(request, 'vendor/vprofile.html', context)
